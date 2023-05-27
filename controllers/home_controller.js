03562de72e93cd7f7ac1cb1c348205e29da5c44d
shubhamchaudhary199522 <chaudharyshubham199522@gmail.com>
2023-05-28 00:02:21 +0530

    views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    return res.end('<h1> express is up for codeial! </h1>');
+    // return res.end('<h1> express is up for codeial! </h1>'); //without views. 
+     return res.render('home',{
+        title: 'Home'
+     });
 }
